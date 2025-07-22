77a432a8f92485537e59c78042423c238d7e1c37
joshua11ke <josueniyonkunda11@gmail.com>
2025-07-22 20:22:15 +0200

    fix: solving conflicts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,3 @@
-// <<<<<<< HEAD
-// import { useState } from "react";
-// import "./Navbar.css";
-
-// const Navbar = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-//   return (
-//     <div className="landing-page">
-//       <div className="navbar-container">
-//         <div className="logo">
-//           <img src="src/assets/logo-smait 1.png" alt="logo" id="navbar-logo" />
-//           <p className="navbar-p">STUDENTSYNC</p>
-//         </div>
-//         <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-//           &#9776;
-//         </div>
-//         <div className={`nav-links ${menuOpen ? "active" : ""}`}>
-//           <h5 className="home-navbar">home</h5>
-//           <h5 className="navbar-add">add student</h5>
-//           <h5 className="navbar-all">all students</h5>
-//         </div>
-//       </div>
-//     </div>
 import { useNavigate, NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -31,7 +8,7 @@ const Navbar = () => {
       <header className="navbar-logo" onClick={navigate("/")}>
         <img src="src/assets/logo-smait 1.png" id="navbar-logo" />
         <span>STUDENT SYNC</span>
-      </header>
+      </header>{" "}
       <ul className="navbar-list">
         <li className="navbar-list-item" onClick={navigate("/")}>
           home
